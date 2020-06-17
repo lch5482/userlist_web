76165d4a092c12bfca93c26fc8ea044fed0ccb09
leechiho <lch5482@gmail.com>
2020-06-17 12:18:41 +0900

    nodejs

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 var express = require('express')
-
+var path = require('path')
 var app = express()
 
+
+app.set('views', path.join(__dirname, '/views'))
+app.set('view engine', 'ejs')
+
+app.use(express.urlencoded())
+app.use(express.json())
+
+app.get('/hello', (request, response)=>{
+    //console.log(request)
+   response.render('test.ejs')
+})  
+
 var port = 8000;
-app.listen(port, function(){
-    console.log(`Server is Runing at http://localhost:8080`)
+app.listen(port, ()=>{
+    console.log(`Server is Runing at http://localhost:8000`)
 })
