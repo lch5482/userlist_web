3960d91ae678784fe3dd424710e8828bb550b150
leechiho <lch5482@gmail.com>
2020-06-17 15:33:42 +0900

    mysql_conection

--- app.js
+++ app.js
@@ -1,20 +1,48 @@
 var express = require('express')
 var path = require('path')
 var app = express()
+var mysql = require('mysql')
 
+
+sql_comfig = {
+    host:'localhost',
+    user:'root',
+    password:'1234',
+    database:'o2'
+}
+var db = mysql.createConnection(sql_comfig)
+db.connect()
 
 app.set('views', path.join(__dirname, '/views'))
 app.set('view engine', 'ejs')
 
-app.use(express.urlencoded())
-app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+app.use(express.json({extended:false}))
 
 app.get('/hello', (request, response)=>{
     //console.log(request)
-   response.render('test.ejs')
+    var name = "leechiho"
+   response.render('test', {data : name})
 })  
+
+app.get('/data',(req, res)=>{
+    var sql = 'SELECT * FROM topic '
+    db.query(sql, (err, result)=>{
+        if(err){
+            console.log(err)
+        }else{
+            console.log(result[0].description)
+           // res.send(result[0].author+"수업은"+result[0].title)
+           //res.send(`${result[0].author} 수업은 ${result[0].title}`)
+            res.render('data', {data: result})
+
+        }
+
+    })
+})
+
 
 var port = 8000;
 app.listen(port, ()=>{
-    console.log(`Server is Runing at http://localhost:8000`)
+    console.log('Server is Runing at http://localhost:8000')
 })
